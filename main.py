ff4fae7ade80aadbaba32453b9cc20ae4ebf276a
vitoradsp <vitoradsp@gmail.com>
2024-01-15 01:58:43 -0300

    Added Get Song by Name function in youtube

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import re
 import tekore as tk
 from sideutil import GetToken
-import ytmusicapi
-
-
+from ytmusicapi import YTMusic
 
 
 class Playlist(GetToken):
-    def __init__(self, user_playlist: str=str(input("Insert Playlist Link: "))):
+    def __init__(self, user_playlist: str=str(input("Insert Playlist Link: ")), all_items:str=None):
         super().__init__()
         self.user_playlist = user_playlist
+        self.all_items = all_items
         self.post_get_token()
         self.get_values_by_playlist_id()
 
@@ -17,22 +16,39 @@ class Playlist(GetToken):
         self.user_playlist = re.split(r'[/|?]', self.user_playlist)
         spotify = tk.Spotify(self.values)
         response = spotify.playlist_items(self.user_playlist[4])
-        a_items = spotify.all_items(response)
+        self.all_items = spotify.all_items(response)
         '''url = f'https://api.spotify.com/v1/playlists/{self.user_playlist[4]}/tracks?fields=items(track(name))'
         headers = {'Authorization' : 'Bearer ' + self.values}
         response = requests.get(
             url,
             headers=headers
             ).json()'''
-        a = open(
-            'results.txt',
-            'w'
-            )
-        for x in a_items:
-            a.write(f"{x.track.name}\n")
 
-        a.close()
+class Get_yt_music(Playlist):
+    def __init__(self):
+        super().__init__()
+        self.search_song()
+    
+    def search_song(self):
+        yt = YTMusic()
+        for x in self.all_items:
+            search_result = yt.search(
+            query=x.track.name,
+            limit=1,
+            ignore_spelling=False
+            )
+            for y in search_result:
+                print(x.track.artists[0])
+                if 'title' in y and y['category'] == 'Top result' and (artistval == f'{x.track.artist}' for artistval in y['artists']):
+                    print(f"{y['title']}\n")
+                else:
+                    pass
+
+
+
+    
            
 
 
 playlist = Playlist()
+get_yt_msc = Get_yt_music()
--- sideutil.py
+++ sideutil.py
@@ -1,18 +1,22 @@
 import base64
 import requests
-
+import json
 
 class GetToken():
     def __init__(self,
                 values:str=None,
-                client_id:str='39497ccb50b540fba237f700796dc4e0', 
-                client_secret:str='9758b13a8ff347a694f897736b17c274',
+                client_id:str=None, 
+                client_secret:str=None,
                 ):
         self.client_id = client_id
         self.client_secret = client_secret
         self.values = values
     
     def post_get_token(self):
+        with open('config.json') as all_data:
+            client_data = json.load(all_data)
+            self.client_id = client_data['client_id']
+            self.client_secret = client_data['client_secret']
         together = self.client_id + ":" + self.client_secret
         encoded_values = together.encode('utf-8')
         encoded_token = str(base64.b64encode(encoded_values), 'utf-8')
