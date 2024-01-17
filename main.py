b78aaf3927042356d3f6543f7d1a46d16f846818
vitoradsp <vitoradsp@gmail.com>
2024-01-17 00:20:41 -0300

    Added spotify to youtube conversion and downloading function

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ import re
 import tekore as tk
 from sideutil import GetToken
 from ytmusicapi import YTMusic
-
+import youtube_dl
 
 class Playlist(GetToken):
-    def __init__(self, user_playlist: str=str(input("Insert Playlist Link: ")), all_items:str=None):
+    def __init__(
+        self,
+        user_playlist: str=str(
+        input("Insert Playlist Link: ")),
+        all_items:str=None
+        ):
         super().__init__()
         self.user_playlist = user_playlist
         self.all_items = all_items
         self.post_get_token()
         self.get_values_by_playlist_id()
+
 
     def get_values_by_playlist_id(self):
         self.user_playlist = re.split(r'[/|?]', self.user_playlist)
@@ -25,10 +31,11 @@ class Playlist(GetToken):
             ).json()'''
 
 class Get_yt_music(Playlist):
-    def __init__(self):
+    def __init__(self, urls:list=[]):
         super().__init__()
+        self.urls = urls
         self.search_song()
-    
+
     def search_song(self):
         yt = YTMusic()
         for x in self.all_items:
@@ -38,17 +45,34 @@ class Get_yt_music(Playlist):
             ignore_spelling=False
             )
             for y in search_result:
-                print(x.track.artists[0])
-                if 'title' in y and y['category'] == 'Top result' and (artistval == f'{x.track.artist}' for artistval in y['artists']):
-                    print(f"{y['title']}\n")
+                if 'title' in y and y['category'] == 'Top result' and 'videoId' in y and 'artists' in y and len(y['artists'])>0 and (artistval == {x.track.artist[0].name} for artistval in y['artists'][0]['name']):
+                    self.urls.append(y['videoId'])
                 else:
                     pass
 
 
-
+class Downloader(Get_yt_music):
+    def __init__(self):
+        super().__init__()
+        self.download_songg()
     
-           
+    def download_songg(self):
+        ydl_opts = {
+            'verbose': True,
+            'format':'bestaudio/best',
+            'postprocessors':[
+            {
+                'key':'FFmpegExtractAudio',
+                'preferredcodec':'mp3',
+                'preferredquality':'192'
+                }
+            ]
+                    }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            for url in self.urls:
+                ydl.download([f'https://www.youtube.com/watch?v={url}'])
 
 
 playlist = Playlist()
 get_yt_msc = Get_yt_music()
+download_song = Downloader()
